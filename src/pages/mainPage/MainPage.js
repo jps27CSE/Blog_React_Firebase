053d3a22750bb08a657ab2b80242da1a1a247dbe
Jack Pritom Soren <jackpritomsoren@hotmail.com>
2022-02-05 16:34:27 +0600

    displaying data from firebase

--- src/pages/mainPage/MainPage.js
+++ src/pages/mainPage/MainPage.js
@@ -1,12 +1,17 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Button, Card, CardImg } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
+import { getDocs, collection } from "firebase/firestore";
+import { db } from "../../firebase/firebaseconfig";
 
 const MainPage = () => {
   const storage = localStorage.getItem("isAuth");
 
   const navigate = useNavigate();
+
+  const [postLists, setPostList] = useState([]);
+  const postCollectionRef = collection(db, "posts");
 
   useEffect(() => {
     if (!storage) {
@@ -14,45 +19,42 @@ const MainPage = () => {
     }
   });
 
-  return (
+  useEffect(() => {
+    const getPosts = async () => {
+      const data = await getDocs(postCollectionRef);
+      setPostList(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    };
+    getPosts();
+  });
+
+  return postLists.length ? (
     <div className="container">
-      <div className="row">
-        <Card style={{ width: "70rem", marginTop: "1%" }}>
-          {/* <Card.Img variant="top" src="holder.js/100px180" /> */}
-          <Card.Body>
-            <Card.Title>Card Title</Card.Title>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </Card.Text>
-            <Link
-              to={`/article/2`}
-              style={{ textDecoration: "none", color: "black" }}
-            >
-              <Button variant="primary">Read More</Button>
-            </Link>
-          </Card.Body>
-        </Card>
-      </div>
-      <div className="row">
-        <Card style={{ width: "70rem", marginTop: "1%" }}>
-          {/* <Card.Img variant="top" src="holder.js/100px180" /> */}
-          <Card.Body>
-            <Card.Title>Card Title</Card.Title>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </Card.Text>{" "}
-            <Link
-              to={`/article/2`}
-              style={{ textDecoration: "none", color: "black" }}
-            >
-              <Button variant="primary">Read More</Button>
-            </Link>
-          </Card.Body>
-        </Card>
-      </div>
+      {postLists.map((post) => {
+        return (
+          <div className="row" style={{ marginLeft: "1%" }}>
+            <Card style={{ width: "40rem", marginTop: "4%" }}>
+              {/* <Card.Img variant="top" src="holder.js/100px180" /> */}
+              <Card.Body>
+                <Card.Title>{post.title}</Card.Title>
+
+                <Card.Text>{post.content}</Card.Text>
+                <Link
+                  to={`/article/2`}
+                  style={{ textDecoration: "none", color: "black" }}
+                >
+                  <Button variant="primary">Read More</Button>
+                </Link>
+                <div style={{ width: "70rem", marginTop: "1%" }}>
+                  Author: @ {post.author.name}
+                </div>
+              </Card.Body>
+            </Card>
+          </div>
+        );
+      })}
     </div>
+  ) : (
+    <div>Empty</div>
   );
 };
 
